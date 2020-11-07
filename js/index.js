59b7efcac7fd71a550ff84682ac1a1146f75ddcd
Journey Park <tenderain@gmail.com>
2020-11-07 13:21:37 +0900

    update module

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
-import Vue from "vue";
-import App from './pages/App'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import App from './pages/App';
+import router from './router';
+import store from './store';
+
+Vue.use(Vuetify);
 
 new Vue({
-    el: '#app',
-    router,
-    store,
-    render: h => h(App)
+  el: '#app',
+  router,
+  store,
+  vuetify: new Vuetify(),
+  render: (h) => h(App),
 });
-
